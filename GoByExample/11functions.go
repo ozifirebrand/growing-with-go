201ab153982ad91d424efe06fdb7ddb95152779b
OziomaOkoroafor <ozioma@crust.africa>
2024-11-22 01:04:07 +0100

    Create functions

---
+++ GoByExample/11functions.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Plus:", plus(4, 8))
+	fmt.Println("Plus plus:", plusPlus(4, 8, -5))
+}
+
+func plus(a int, b int) int {
+	return a + b
+}
+
+func plusPlus(a int, b int, c int) int {
+	return a + b + c
+}
